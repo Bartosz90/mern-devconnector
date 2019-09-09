4f2ddd648eede078f02be53fa5d7932297bc2a5e
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-09 18:23:17 +0200

    initial server and DB setup, add routes, JWT auth

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const { check, validationResult } = require("express-validator");
 
 const app = express();
+
+//connect DB
+connectDB();
+
+//Init Middleware
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API running"));
+
+//define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.get("/", (req, res) => res.send("API running"));
