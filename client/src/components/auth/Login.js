7fb73f9ece318599d476eda4bd10d17d6ca03fdf
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-26 16:35:34 +0200

    add front-end auth

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: ""
@@ -14,9 +17,14 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-
-    console.log("success");
+    login(email, password);
   };
+
+  //Redirect if logged in
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashborad" />;
+  }
 
   return (
     <>
@@ -53,4 +61,16 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
 import { register } from "../../actions/auth";
 // import axios from "axios";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -45,6 +45,12 @@ const Register = ({ setAlert, register }) => {
       //   }
     }
   };
+
+  //Redirect if logged in
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashborad" />;
+  }
 
   return (
     <>
@@ -104,10 +110,15 @@ const Register = ({ setAlert, register }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { setAlert, register }
 )(Register);
